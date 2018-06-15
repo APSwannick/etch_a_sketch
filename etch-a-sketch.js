72dfa110160932d6fade7e3cdbb035a842b9fad0
Patrick Swannick <pswannic326@gmail.com>
2018-06-15 17:51:37 -0400

    Add Shake feature

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -30,12 +30,12 @@ function draw_canvas(gridsize) {
   canvasdiv.innerHTML = "";
   for (i = 0; i < gridsize; i++) {
     let new_row = document.createElement("div");
-    new_row.id = ("row-" + i);
+    //new_row.id = ("row_" + i);
     new_row.classList.add("canvas_row")
     canvasdiv.appendChild(new_row);
     for (j = 0; j < gridsize; j++) {
       let new_cell = document.createElement("div");
-      new_cell.id = ("row-" + i + "-col-" + j);
+      //new_cell.id = ("row_" + i + "-col_" + j);
       new_cell.classList.add("canvas_cell");
       new_cell.addEventListener("mouseover", cell_mouseover);
       new_row.appendChild(new_cell);
@@ -43,12 +43,23 @@ function draw_canvas(gridsize) {
   }
 }
 
+function On_Delay_Remove_Shake(elem) {
+  setTimeout(function () {
+    elem.className = elem.className.replace(' shake', '');
+  }, 500);
+}
+
+
 //Setup Buttons & Textboxes
 document.getElementById("btn_reset")
   .addEventListener("click", function () {
-    let user_gridsize = document.querySelector('input[name="rng_slide"]').value;
+    let user_gridsize = 16;
+    //let user_gridsize = document.querySelector('input[name="rng_slide"]').value;
     console.log(user_gridsize);
     draw_canvas(user_gridsize);
+    canvas_elem = document.getElementById('canvas')
+    canvas_elem.className += " shake";
+    On_Delay_Remove_Shake(canvas_elem);
   });
 
 document.getElementById("btn_color_black")
@@ -66,5 +77,6 @@ document.getElementById("btn_color_fade")
     Colormode = "fade";
   });
 
-document.querySelector('input[name="rng_slide"]').value = gridsize;
+
+//document.querySelector('input[name="rng_slide"]').value = gridsize;
 draw_canvas(gridsize);
